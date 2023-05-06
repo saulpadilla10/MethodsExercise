d40be1375ba3855edc5e832b48f226a9c1ac5d39
saulpadilla10 <saulp1021@yahoo.com>
2023-05-05 20:23:47 -0700

    start of Methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, my name is DragonHawk! What is your name?");
+            var myName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {myName}, what color is your shirt?");
+            var myShirt = Console.ReadLine();
+
+            Console.WriteLine($"Wow! {myShirt} is my favorite color! What is your favorite animal?");
+            var myPet = Console.ReadLine();
+
+            Console.WriteLine($"Woah. {myPet} are my favorite too! What's you favorite band?");
+            var myBand = Console.ReadLine();
+
+            Console.WriteLine($"No. Way. {myBand} is my favorite! Did we just become best friends?!");
+           
         }
     }
 }
