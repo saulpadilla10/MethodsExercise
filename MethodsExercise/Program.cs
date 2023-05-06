f19b5e3283351eaed3c9060b40d5c3a87af404e1
saulpadilla10 <saulp1021@yahoo.com>
2023-05-05 22:45:34 -0700

    Finale

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,48 @@
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        public static int Alltogether(int num1, int num2, int num3, int num4)
+        {
+            return num1 + num2 * num3 / num4;
+        }
+
+
         static void Main(string[] args)
         {
+
+            var answerTrue = Sum(2, 6);
+            var idkSomething = Multiply(60, 2, 4);
+            var release = Subtract(1000, 1);
+            var share = Divide(10000, 3);
+            var savings = Modulus(100, 33);
+            var tour = Alltogether(2, 4, 6, 8);
+
             Console.WriteLine("Hello, my name is DragonHawk! What is your name?");
             var myName = Console.ReadLine();
 
@@ -18,12 +58,14 @@
 
             Console.WriteLine($"No. Way. {myBand}?!");
             Console.WriteLine($"So, what I learned from this, {myName}, is:");
-          
+
             Console.WriteLine($"1. Your shirt color, {myShirt}. My favorite.");
             Console.WriteLine($"2. Favorite animal: {myPet}. My favorite.");
             Console.WriteLine($"3. Favorite Band: {myBand}. Also mine.");
             Console.WriteLine("And we are now best friends.");
 
         }
+
     }
+
 }
